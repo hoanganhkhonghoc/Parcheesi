7d2c65230077b7255f15fcff776b1fd5cdec0d63
hoanganhkhonghoc <hoanganhkhonghoc@gmail.com>
2023-12-14 11:40:12 +0700

    HoangAnh: Tạo Project, Import Model, Làm Map và tách Prefab cần thiết.

---
+++ Assets/Game/Scripts/Map/Map.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public Transform[] listTransMove;
+    public MoveHome[] listMoveHome;
+    public NguaTrongChuong[] listNgua;
+}
---
+++ Assets/Game/Scripts/Map/MoveHome.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MoveHome
+{
+    public Transform[] trans;
+    public ColorEnum color;
+}
+[System.Serializable]
+public class NguaTrongChuong
+{
+    public Transform[] trans;
+    public ColorEnum color;
+}
